2d403fda7229c792bf09013e4fd470b316b85d04
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2020-02-26 11:51:18 +0100

    add utf8 declarations (PEP 263)

--- bin/downloads.py
+++ bin/downloads.py
@@ -2,7 +2,7 @@
 #! /usr/bin/env python
 
 #
-# Copyright (c) 2014-2020 by Pawel Tomulik
+# Copyright (c) 2014-2020 by Paweł Tomulik
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
